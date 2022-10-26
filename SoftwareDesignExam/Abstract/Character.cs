3dab580968075113e2d571df503d0d4ed9f057e8
bjarte <22171847+freebattie@users.noreply.github.com>
2022-10-26 23:07:26 +0200

    prepered for decorator pattern

--- SoftwareDesignExam/Abstract/Character.cs
+++ SoftwareDesignExam/Abstract/Character.cs
@@ -2,19 +2,28 @@
 using SoftwareDesignExam.Interface;
 
 namespace SoftwareDesignExam.Abstract {
-    public abstract class Character
-    {
+    public abstract class Character {
         public string Name { get; set; }
-        public double Health { get; set; }
-        public double Level { get; set; }
-        public double Crit { get; set; }
+
+        private const double multiplayer = 0.05;
+
+        public double Health { get => health; set => health = CalculateHealth(value); }
+
+        private double CalculateHealth(double value) {
+            return Math.Round((Level * multiplayer * value) + value, 0);
+        }
+
+        public double Level { get => level; set => level = value; }
+        public double Crit { get => crit; set => crit = value; }
         public double ArmorLevel { get; set; }
         protected IWeapon Weapon { get => _weapon; private set => _weapon = value; }
 
         IWeapon _weapon;
+        private double crit;
+        private double level;
+        private double health;
 
-        public void SetWeapon(IWeapon weapon)
-        {
+        public void SetWeapon(IWeapon weapon) {
             Weapon = weapon;
         }
 
