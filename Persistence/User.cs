8ef7d1224e346b83055d690280d113d213e16524
Ellaoui94 <abab010@student.kristiania.no>
2022-10-29 17:29:15 +0200

    Added v1 of ModelDiagram.puml and Connection to database with SQLite with a User class

---
+++ Persistence/User.cs
@@ -0,0 +1,26 @@
+﻿namespace Persistence;
+
+public class User
+{
+    private string name;
+    private int level;
+    private int topscore;
+
+    public string Name
+    {
+        get => name;
+        set => name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Level
+    {
+        get => level;
+        set => level = value;
+    }
+
+    public int Topscore
+    {
+        get => topscore;
+        set => topscore = value;
+    }
+}
