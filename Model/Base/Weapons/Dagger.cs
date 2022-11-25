44920633b35cb6adbe538aefdcecdf83a6d55ed6
bjarte <22171847+freebattie@users.noreply.github.com>
2022-11-25 23:05:34 +0100

    last update

---
+++ Model/Base/Weapons/Dagger.cs
@@ -0,0 +1,30 @@
+﻿using Model.Base.Weapons.Abstract;
+using Model.Decorator.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Base.Weapons {
+    internal class Dagger : Weapon {
+        private CharacterInfo? _target;
+        public Dagger() : base() {
+           
+            Description = "does 3x damage if enemy healt is below 25%";
+        }
+        public override double GetDamage() {
+            var life = _target?.GetHealth();
+            var max = _target?.GetMaxHealth();
+            if ((life / max) *100 <= 25) {
+                return Damage * 3;
+            }
+            else
+                return Damage;
+        }
+
+        public override void SetTarget(CharacterInfo target) {
+            _target = target;
+        }
+    }
+}
---
+++ Model/Base/Weapons/LongSword.cs
@@ -0,0 +1,65 @@
+﻿using Model.Base.Weapons.Abstract;
+using Model.Decorator.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Model.Base.Weapons {
+    internal class LongSword: Weapon {
+        #region private fileds
+        private CharacterInfo? _target;
+        private Weapon? _enemyWeapon;
+        private int _counter = 0;
+        #endregion
+
+        #region constructor
+        public LongSword() : base() {
+            _target = null;
+            Description = "You have a 100% chance to disarm a enemy";
+        }
+        #endregion
+
+        #region overrides
+        public override double GetDamage() {
+            if (_target == null) {
+                CheckIfYouDisarmEnemy();
+
+            }
+
+            return Damage;
+        }
+
+
+        public override void SetTarget(CharacterInfo target) {
+            this._target = target;
+        }
+        #endregion
+
+        #region private methods
+        private void CheckIfYouDisarmEnemy() {
+            if (_counter == 0) {
+                Random random = new Random();
+                var chance = random.Next(100);
+                if (chance <= 100) {
+                    _enemyWeapon = _target?.GetWeapon();
+                    _target?.SetWeapon(new NoWeapon());
+                }
+
+            }
+            else if (_counter == 4) {
+                if (_enemyWeapon != null) {
+                    _target?.SetWeapon(_enemyWeapon);
+                    _target = null;
+                    _counter = 0;
+                }
+            }
+            else {
+                _counter++;
+            }
+        }
+        #endregion
+    }
+}
